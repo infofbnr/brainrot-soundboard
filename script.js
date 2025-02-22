dd3cc238049ec3021646509fdf4a6f81d309bddd
infofbnr <crazyjim0123@gmail.com>
2025-02-22 15:07:22 +0200

    added 8 new buttons

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ const sounds = {
     sound5: new Audio('sounds/sound5.mp3'),
     sound6: new Audio('sounds/sound6.mp3'),
     sound7: new Audio('sounds/sound7.mp3'),
-    sound8: new Audio('sounds/sound8.mp3')
+    sound8: new Audio('sounds/sound8.mp3'),
+    sound9: new Audio('sounds/sound9.mp3'),
+    sound10: new Audio('sounds/sound10.mp3'),
+    sound11: new Audio('sounds/sound11.mp3'),
+    sound12: new Audio('sounds/sound12.mp3'),
+    sound13: new Audio('sounds/sound13.mp3'),
+    sound14: new Audio('sounds/sound14.mp3'),
+    sound15: new Audio('sounds/sound15.mp3'),
+    sound16: new Audio('sounds/sound16.mp3')
 };
 
 let currentSound = null; // Track the currently playing sound
